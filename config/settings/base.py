fb01e7c5141b78ada87504ee527279f4910a9d35
aka-radix <h.radiahmad@gmail.com>
2024-05-03 13:50:49 +0300

    feat: add base project structure

---
+++ config/settings/base.py
@@ -0,0 +1,113 @@
+from pathlib import Path
+
+import environ
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+# ENV
+
+env = environ.Env()
+
+environ.Env.read_env(BASE_DIR / ".env")
+
+# GENERAL
+
+SECRET_KEY = env.str("DJANGO_SECRET_KEY")
+
+DEBUG = env.bool("DJANGO_DEBUG")
+
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS")
+
+APPS_DIR = BASE_DIR / "glasses"
+
+WSGI_APPLICATION = "config.wsgi.application"
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+ROOT_URLCONF = "config.urls"
+
+# LOCALIZATION AND INTERNATIONALIZATION
+
+LANGUAGE_CODE = "en-us"
+
+TIME_ZONE = "Asia/Amman"
+
+USE_I18N = True
+
+USE_TZ = True
+
+# Application definition
+
+DJANGO_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+]
+
+THIRD_PARTY_APPS = []
+
+LOCAL_APPS = []
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
+
+# MIDDLEWARE
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+# TEMPLATES
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+# DATABASE
+
+DATABASES = {
+    "default": env.db("DATABASE_URL"),
+}
+
+DATABASES["default"]["ATOMIC_REQUESTS"] = True
+
+# PASSWORD VALIDATION
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",  # noqa: E501
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",  # noqa: E501
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",  # noqa: E501
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",  # noqa: E501
+    },
+]
+
+# STATIC FILES (CSS, JavaScript, images)
+
+STATIC_URL = "/static/"
